78c1b60d08a9f3d4fab6fc96e7da8a0397831d1c
allen lim <allenman23@yahoo.com>
2018-09-25 18:54:30 -0500

    added code to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include "clock.h"
+#include<iostream>
+#include<thread>
+
 int main() 
 {
 	Clock clock;
-	clock.display_time();
+	int i{ 0 };
+	while (i != 100)
+	{
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		clock.display_time();
+		std::cout << std::endl;
+		++i;
+	}
+//	clock.display_time();
 	return 0;
 }
